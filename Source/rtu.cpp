d725ac9092b6186c256f26764c5e95be3548ff2d
Max Houck <mmhww8@mail.missouri.edu>
2017-12-05 13:25:44 -0600

    made some pseudocode for main on rtu

--- Source/rtu.cpp
+++ Source/rtu.cpp
@@ -40,3 +40,25 @@ void AnalogInput::test_ADC() {
 
 //max made a change to test github
 //max made another change to test github
+
+
+//pthread
+//needs to run at 1khz
+//read adc
+
+int main {
+  //instantiate all objects
+    //analog inputs
+    //digital inputs
+    //digital outputs
+    //networking stuff
+    
+    
+  while(1) {  
+  //monitor flags from inputs
+   //if (flag)
+     // //grab states and write to a log file
+      
+  }
+
+}
