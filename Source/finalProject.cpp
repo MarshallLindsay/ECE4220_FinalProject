84bf9908c49e07a42d6c2f904d81c7a33b47c6f0
Marshall Lindsay <mbanks1050@gmail.com>
2017-12-07 14:07:34 -0600

    So for some reason I cant add a comma to a string

--- Source/finalProject.cpp
+++ Source/finalProject.cpp
@@ -73,7 +73,7 @@ SocketCommunication::~SocketCommunication(){
 int SocketCommunication::sendMessage(struct logEntry buffer){
   int n;
   string temp;
-  temp = "hello" + buffer.analoginstate + ",";
+  temp = "hello" + buffer.analoginstate + " , ";
   /*
   + "," + buffer.digin1state + "," + buffer.digin2state + ",";
  temp += buffer.digin3state + "," + buffer.digout1state + "," + buffer.digout2state + ",";
