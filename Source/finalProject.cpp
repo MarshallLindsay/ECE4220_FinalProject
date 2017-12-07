c5bbf704e3a9caced6ffa77021f60206c0504318
Marshall Lindsay <mbanks1050@gmail.com>
2017-12-06 20:35:44 -0600

    Attempted to fix the bind() function (2)

--- Source/finalProject.cpp
+++ Source/finalProject.cpp
@@ -9,47 +9,56 @@ ECE 4220 Final Final Project
 
 //Socket communication methods
 SocketCommunication::SocketCommunication(){
+  int sockfd1, newsockfd, portno1, pid, j = 0;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+  int boolval1;
 
   //Create a socket, connectionless
-  this->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockfd1 = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sockfd < 0){
 		cout<<"\nSocket creation failed"<<endl;
 		exit(1);
 	}
 
   //Set the port.. We could change to dymanic port
-  this->portno = 2345;
+  portno1 = 2345;
   //cout<<"hello"<<this->portno<<endl;
+  this->portno = protno1;
 
   //Set the boolval.. Just gotta do it
   this->boolval = 1;
 
   //Clear all of the server data
   this->length = sizeof(this->serveraddress);
-  bzero(&this->serveraddress, this->length);
+  bzero((char*)&serv_addr, sizeof(serv_addr));
 
   //Set the address family as IPv4
-  this->serveraddress.sin_family = AF_INET;
+  serv_addr.sin_family = AF_INET;
 
   //Set the address to INADDR_ANY
-  this->serveraddress.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
 
   //Set the port number
-  this->serveraddress.sin_port = htons(this->portno);
+  serv_addr.sin_port = htons(portno1);
 
   //Bind the socket
-  if(bind(this->sockfd, (struct sockaddr *)&(this->server), sizeof(this->serveraddress)) < 0){
+  if(bind(sockfd1, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
 		cout<<"\nFailed to bind the socket!"<<endl;
 		exit(1);
 	}
 
   //Set the option for broadcasting
-  if (setsockopt(this->sockfd, SOL_SOCKET, SO_BROADCAST, &this->boolval, sizeof(this->boolval)) < 0)
+  if (setsockopt(sockfd1, SOL_SOCKET, SO_BROADCAST, &boolval1, sizeof(boolval1)) < 0)
 	{
 		cout<<"\nError setting socket options"<<endl;
 		exit(1);
 	}
 
+  this->sockfd = sockfd1;
+  this->portno = portno1;
+  this->boolval = boolval1;
+  this->serveraddress = serv_addr;
 
   //Get the ip of the system
   //Pulled from stackoverflow. Modified slightly for my uses
