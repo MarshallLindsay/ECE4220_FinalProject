d45af8b48ed823f8936969d7dd1e933a84797041
Marshall Lindsay <mbanks1050@gmail.com>
2017-12-07 22:07:43 -0600

    debugging messages

--- Source/historian.cpp
+++ Source/historian.cpp
@@ -124,6 +124,7 @@ void* readMessages(void* ptr){
 		//Filter messages
 		if(buffer[0] != '#'){
 			//Create a log entry of the message
+			cout<<"About to do the log thing : "<<buffer<<endl;
 			createLogEntry(buffer);
 		}else{
 			cout<<"Master message received"<<endl;
